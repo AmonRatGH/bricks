cd1c03d4b5df5b93a638d47b1912a6a03b5d573f
AmonRatGH <59555511+AmonRatGH@users.noreply.github.com>
2020-03-14 18:00:55 +0100

    improvements
    improvements

--- js/game.js
+++ js/game.js
@@ -1,47 +1,68 @@
 var c = document.getElementById("bricks");
 var ctx = c.getContext("2d");
-var starX=325;
-var startY=575;
+var lives=3;
+var bricks={
+	
+};
+
+var player={
+	x:325,
+	y:575,
+};
 var start=false;
-var x=325;
-var x1 = c.width/2;
-var y1 = c.height-30;
-var dx = 2;
-var dy = -2;
+var test={
+	x:10,
+	y:20,
+};
+
+var ball={
+	x: c.width/2,
+	y: c.height-30,
+};
+var ballMove={
+	dx: -2,
+	dy: -2,
+};
+//var ball.x = c.width/2;
+//var ball.y = c.height-30;
+//var ballMove.dx = 2;
+//var ballMove.dy = -2;
 var radius=8;
 
 function drawPlayer(){
 	document.getElementById("start").disabled = true;
 	ctx.beginPath();
-	ctx.rect(starX, startY, 150, 15);
+	ctx.rect(player.x, player.y, 150, 15);
 	ctx.lineWidth=2;
 	ctx.fillStyle = "#000000";
 	ctx.strokeStyle = "gray";
 	ctx.fill();
 	ctx.stroke();
 	start=true;
-	console.log(start);
 }
 
-function ballMove(){
+function ballMoveFunction(){
 	if(start==true){
-		ctx.clearRect(x1-10, y1-10, radius*2+4, radius*2+4);
+		ctx.clearRect(0, 0, c.width, c.height);
 		drawBall();
-		if(x1 + dx > c.width || x1 + dx < 0) {
-			dx = -dx;
+		if(ball.x + ballMove.dx > c.width || ball.x + ballMove.dx < 0) {
+			ballMove.dx = -ballMove.dx;
 		}
 
-		if(y1 + dy > c.height || y1 + dy < 0) {
-			dy = -dy;
+		if(ball.y + ballMove.dy > c.height || ball.y + ballMove.dy < 0) {
+			ballMove.dy = -ballMove.dy;
 		}
-		x1 += dx;
-		y1 += dy;
+		if((ball.x>=player.x&&ball.x<=player.x+150)&&ball.y+1==player.y){
+			ballMove.dy = -ballMove.dy;
+		}
+		ball.x += ballMove.dx;
+		ball.y += ballMove.dy;
 	}
 }
 
 function drawBall(){
     ctx.beginPath();
-    ctx.arc(x1, y1, 8, 0, Math.PI*2);
+    ctx.arc(ball.x, ball.y, 8, 0, Math.PI*2);
     ctx.fillStyle = "gray";
     ctx.fill();
     ctx.closePath();
@@ -51,7 +72,9 @@ function resetGame(){
 	document.getElementById("start").disabled = false;
 	ctx.clearRect(0, 0, c.width, c.height);
 	start=false;
-	x=350;
+	player.x=350;
+	clearInterval(int1);
+	clearInterval(int2);
 	console.log(start);
 }
 
@@ -64,7 +87,6 @@ function moveLeft(x){
 		ctx.strokeStyle = "gray";
 		ctx.fill();
 		ctx.stroke();
-		console.log(x);
 	}
 }
 
@@ -77,31 +99,62 @@ function moveRight(x){
 		ctx.strokeStyle = "gray";
 		ctx.fill();
 		ctx.stroke();
-		console.log(x);
 	}
 }
 
 function ballInterval(){
-	setInterval(ballMove,20);
+	createBricks();
+	var int1 = setInterval(ballMoveFunction,10);
+	var int2 = setInterval(drawPlayer,10);
+	setInterval(createBricks,10);
+}
+
+function createBricks(){
+	var bricks = new Array(5);
+	var value =62;
+	for (var i = 0; i < bricks.length; i++) { 
+		bricks[i] = new Array(14); 
+	}
+	for (var i = 0; i < bricks.length; i++) { 
+		for (var j = 0; j < bricks[i].length; j++) { 
+			bricks[i][j] = {
+				x:j*value,
+				y:i*(value/2) ,
+				pop: false,
+			};
+		} 
+	}
+	
+	for (var k = 0; k < bricks.length; k++) { 
+		for (var l = 0; l < bricks[k].length-1; l++) { 
+			if(bricks[k][l].pop==true){
+				continue;
+			}else{
+				ctx.beginPath();
+				ctx.strokeStyle = "#FF0000";
+				ctx.fillStyle = "#0000ff";
+				ctx.rect(bricks[k][l].x, bricks[k][l].y, 57, 28);
+				ctx.stroke();
+			}
+		} 
+	} 
 }
 
 document.onkeydown = function(event) {
 	if(start==true){
 		if(event.keyCode == 37){
-			if(x<0){
-				x=2;
+			if(player.x<0){
+				player.x=2;
 			}
-			moveLeft(x);
-			x=x-8;
-			console.log("hello");
+			moveLeft(player.x);
+			player.x=player.x-8;
 		}
 		if(event.keyCode == 39){
-			if(x>646){
-				x=646;
+			if(player.x>646){
+				player.x=646;
 			}
-			moveRight(x);
-			x=x+8;
-			console.log("hi");
+			moveRight(player.x);
+			player.x=player.x+8;
 		}
 	}
 }
